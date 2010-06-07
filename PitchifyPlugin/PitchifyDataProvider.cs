40d5029f9fc5e25bda0f2f1d40971384e25bad2d
Geir-Tore Lindsve <lindsve@gmail.com>
2010-06-07 08:22:47 +0200

    Added putting mock reviews on the timeline from the data provider

--- PitchifyPlugin/PitchifyDataProvider.cs
+++ PitchifyPlugin/PitchifyDataProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.Composition;
+using System.Threading;
 using Seesmic.Sdp.Extensibility;
 using Seesmic.Sdp.Utils;
 
@@ -10,17 +11,35 @@ namespace PitchifyPlugin
     {
         private ISidebarAction _sidebarAction;
         private Timeline _timeline;
-
-        public void OnItemProcessed(ProcessingResult result)
-        {
-
-        }
+        private Timer _timer;
 
         public event EventHandler<TimelineItemEventArgs> ItemAdded;
         public event EventHandler<TimelineItemEventArgs> ItemRemoved;
         public void Start()
         {
             _timeline = new Timeline();
+            _timer = new Timer(OnTimerTick, null, 0, 1000);
+        }
+
+        private void OnTimerTick(object state)
+        {
+            SynchronizationHelper.Post(OnItemAdded, new TimelineItemEventArgs(new TimelineItemContainer(new PitchifyReview())));
+        }
+
+        private void OnItemAdded(TimelineItemEventArgs e)
+        {
+            var handler = ItemAdded;
+
+            if (handler != null)
+                handler(this, e);
+        }
+
+        public void OnItemProcessed(ProcessingResult result)
+        {
+            if (result.Blocked == false && result.Error == null)
+            {
+                _timeline.Add(result.TimelineItemContainer);
+            }
         }
 
         public void Stop()
