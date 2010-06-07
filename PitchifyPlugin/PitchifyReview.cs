0c0f3f861d8a871ebf4b85960087f593d47b56b6
Geir-Tore Lindsve <lindsve@gmail.com>
2010-06-07 08:21:40 +0200

    Added timeline item for pitchify reviews

---
+++ PitchifyPlugin/PitchifyReview.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using Seesmic.Sdp.Extensibility;
+
+namespace PitchifyPlugin
+{
+    internal class PitchifyReview : ITimelineItem
+    {
+        public PitchifyReview()
+        {
+            Id = Guid.NewGuid().ToString();
+            Text = "Pitchify";
+        }
+
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Text { get; set; }
+
+        public Uri AvatarUri { get { return null; } }
+
+        public DataTemplate Template { get { return PitchifyPlugin.TimelineItemTemplate; } }
+
+        public DataTemplate NotificationTemplate { get { return null; } }
+
+        public DateTimeOffset DateTime { get; set; }
+
+        public GeoLocation GeoLocation { get { return null; } }
+    }
+}
